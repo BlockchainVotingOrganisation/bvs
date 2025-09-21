c7aac1a44732f1df7bff699bbddd3b7dd79c41d2
luzi41 <louis.goettertz@gmail.com>
2025-09-21 10:23:47 +0200

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+<<<<<<< HEAD
+    root: "/home/louis/Projekte/nextFE/bvs",
+=======
+    root: "/home/louis/Projekte/BlockchainVotingSystem/election-system/nextFE/bvs",
+>>>>>>> 85b3791 (Initial commit.)
+  },
+  reactStrictMode: true,
+  //swcMinify: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  distDir: 'out',
+  // Für externe API Calls (Blockchain) benötigst du keine CSP Einschränkungen
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none'
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
